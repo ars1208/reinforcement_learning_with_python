a87b2a79a2f8581c1fbe832c708031ee81355630
ars1208 <ara.kaz1208@gmail.com>
2020-02-14 17:14:02 +0900

    code update

--- day2/bellman_equation.py
+++ day2/bellman_equation.py
@@ -1,4 +1,11 @@
 def V(s, gamma=0.99):
     V = R(s) + gamma * max_V_on_next_state(s)
     return V
-    
+
+def R(s):
+    if s == "happy_end":
+        return 1
+    elif s == "bad_end":
+        return -1
+    else:
+        return 0
